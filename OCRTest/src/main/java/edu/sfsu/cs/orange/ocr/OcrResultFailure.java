698f5668be948a25e156c34fc1d98c159053edb3
reetvik7 <51454104+reetvik7@users.noreply.github.com>
2019-06-16 02:54:48 +0530

    Final commit

---
+++ OCRTest/src/main/java/edu/sfsu/cs/orange/ocr/OcrResultFailure.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Niket.
+ */
+package edu.sfsu.cs.orange.ocr;
+
+/**
+ * Class to hold metadata for failed OCR results.
+ */
+public final class OcrResultFailure {
+  private final long timeRequired;
+  private final long timestamp;
+  
+  OcrResultFailure(long timeRequired) {
+    this.timeRequired = timeRequired;
+    this.timestamp = System.currentTimeMillis();
+  }
+  
+  public long getTimeRequired() {
+    return timeRequired;
+  }
+  
+  public long getTimestamp() {
+    return timestamp;
+  }
+  
+  @Override
+  public String toString() {
+    return timeRequired + " " + timestamp;
+  }
+}
